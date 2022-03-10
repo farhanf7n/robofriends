7f4ce7476327f48d1f2ca1c681420e664ee2f8d5
Farhan Ahmed <farhanf7n@Farhans-MacBook-Pro.local>
2022-03-10 16:36:33 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+    searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({ searchfield:event.target.value }) 
+    }
+
+    render (){
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+        })
+        return (
+            <div className='tc'>
+                <h1 className='f1'>Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots = {filteredRobots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
