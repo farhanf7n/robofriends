70db28ee421ca38c93807d886eea837f6b70cf6f
Farhan Ahmed <farhanf7n@Farhans-MacBook-Pro.local>
2022-03-13 12:05:38 +0500

    Changed file paths

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 export class App extends Component {
